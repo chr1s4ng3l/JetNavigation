5c4a5bf1b971d5f29e286e4069f6903c76e0519e
Consultant <christopher.tamayocazarez@gmail.com>
2023-04-05 23:20:22 -0400

    Navigation with optional parameter

--- app/src/main/java/com/tamayo/jetnavigation/MainActivity.kt
+++ app/src/main/java/com/tamayo/jetnavigation/MainActivity.kt
@@ -19,9 +19,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.tamayo.jetnavigation.ui.theme.JetNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,12 +42,28 @@ class MainActivity : ComponentActivity() {
                         navController = navigationController,
                         startDestination = Routes.Screen1.route
                     ) {
-                        composable(Routes.Screen1.route) {
-                            ScreenOne(navigationController)
+                        composable(Routes.Screen1.route) { ScreenOne(navigationController) }
+                        composable(Routes.Screen2.route) { ScreenTwo(navigationController) }
+                        composable(Routes.Screen3.route) { ScreenThree(navigationController) }
+                        composable(
+                            Routes.Screen4.route,
+                            arguments = listOf(navArgument("age") { type = NavType.IntType })
+                        ) { backStackEntry ->
+                            ScreenFour(
+                                navigationController,
+                                backStackEntry.arguments?.getInt("age") ?: 0
+
+                            )
                         }
 
-                        composable(Routes.Screen2.route) {
-                            ScreenTwo(navigationController)
+                        composable(
+                            Routes.Screen5.route,
+                            arguments = listOf(navArgument("name") {defaultValue = "Sin Nombre"})
+                        ) { backStackEntry ->
+                            ScreenFive(
+                                navController = navigationController,
+                                name = backStackEntry.arguments?.getString("name")
+                            )
                         }
                     }
 
@@ -80,10 +98,66 @@ fun ScreenTwo(navController: NavController) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Blue)
-            .clickable { navController.navigate(Routes.Screen1.route) }
+            .clickable { navController.navigate(Routes.Screen3.route) }
     ) {
         Text(
             text = "Screen Two 2",
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 32.sp
+        )
+    }
+
+}
+
+@Composable
+fun ScreenThree(navController: NavController) {
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Yellow)
+            .clickable { navController.navigate(Routes.Screen4.createRoute(24)) }
+    ) {
+        Text(
+            text = "Screen Three 3",
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 32.sp
+        )
+    }
+}
+
+@Composable
+fun ScreenFour(navController: NavController, name: Int) {
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .clickable { navController.navigate(Routes.Screen5.createRoute("Christopher")) }
+    ) {
+        Text(
+            text = "I am $name",
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 32.sp
+        )
+    }
+
+}
+
+@Composable
+fun ScreenFive(navController: NavController, name: String?) {
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .clickable { }
+    ) {
+        Text(
+            text = "My name is $name",
             color = Color.White,
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.ExtraBold,
--- app/src/main/java/com/tamayo/jetnavigation/Routes.kt
+++ app/src/main/java/com/tamayo/jetnavigation/Routes.kt
@@ -1,6 +1,13 @@
 package com.tamayo.jetnavigation
 
-sealed class Routes(val route: String){
-    object Screen1: Routes("ScreenOne")
-    object Screen2: Routes("ScreenTwo")
+sealed class Routes(val route: String) {
+    object Screen1 : Routes("ScreenOne")
+    object Screen2 : Routes("ScreenTwo")
+    object Screen3 : Routes("ScreenThree")
+    object Screen4 : Routes("ScreenFour/{age}") {
+        fun createRoute(age: Int) = "ScreenFour/$age"
+    }
+    object Screen5 : Routes("ScreenFive?name={name}") {
+        fun createRoute(name: String) = "ScreenFive?name=$name"
+    }
 }
